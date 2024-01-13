6684aea5b190b0bde04ade1f956a0b0816ce2f4f
agathebeucher <agathe.beucher@ensta-paris.fr>
2024-01-13 19:16:21 +0100

    a

--- src_file/ArchiveReader.cpp
+++ src_file/ArchiveReader.cpp
@@ -1,30 +1,41 @@
 #include "header_file/ArchiveReader.h"
+
+//Constructeur de la classe Archive reader
 ArchiveReader::ArchiveReader(){}
+
+//Destructeur de la classe
 ArchiveReader::~ArchiveReader(){}
+
+
 bool ArchiveReader::loadArchivedFiles(std::string file_path)
 {
 	offset_cache.clear();
 	archive_path = file_path;
+	//'file_type_pos' cherche l'indice de l'information du type de fichier
+	//'file_type_pos' contiend la position de la dernière occurrence de "." dans la chaîne 'archive_path'
 	size_t file_type_pos = archive_path.rfind(".");
+	//création de 'file_type' contenant le TYPE DE FICHIER
 	std::string file_type(archive_path.begin() + file_type_pos + 1, archive_path.end());
 	std::transform(file_type.begin(), file_type.end(), file_type.begin(), ::tolower);
+	
+	//Si le type de fichier est "png", "jpg" ou "bmp", cela signifie qu'il s'agit d'une image unique. 
 	if (file_type == "png" || file_type == "jpg" || file_type == "bmp") {
 		page_num_total = 1;
 		isSingleImg = true;
-
 		return true;
 	}
 
-	//cbr cbz
+	//Sinon, le type de fichier indique une archive cbr ou cbz
 	isSingleImg = false;
-	char *path = &archive_path[0];
-	stream = ar_open_file(path);
-	if (stream == NULL) return false;
-	ar = ar_open_any_archive(stream, strrchr(path, '.'));
-	if (!ar) return false;
+	char *path = &archive_path[0]; //adresse mémoire du premier caractère de la chaîne du chemin du fichier
+	stream = ar_open_file(path);//voir bibliothèque unarr, renvoie un pointeur vers un flux associé au fichier
+	if (stream == NULL) return false;//echec
+	ar = ar_open_any_archive(stream, strrchr(path, '.'));//ouverture de l'archive, renvoie pointeur vers archive ouverte
+	if (!ar) return false;//echec
 	int count = 0;
-	while (ar_parse_entry(ar)) {
-		size_t offset = ar_entry_get_offset(ar);
+	while (ar_parse_entry(ar)) //parcours les entrées de l'archive
+	{
+		size_t offset = ar_entry_get_offset(ar);//offset = position relative dans l'archive
 		offset_cache.push_back(offset);
 		count++;
 	}
