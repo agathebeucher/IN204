58d922afc3186b82ee2655545dfed3b308efb880
agathebeucher <agathe.beucher@ensta-paris.fr>
2024-02-24 22:46:19 +0100

    comment

--- ComicBookReader/src/book.cpp
+++ ComicBookReader/src/book.cpp
@@ -9,40 +9,47 @@ DEFINITION DE LA CLASSE "BOOK" et de ses méthodes
 #include <QPixmap>
 #include <QMessageBox>
 
-Book::Book() : QObject(), pathToDir(""),
-    currPage(0), totalPage(0), ratio(QString("Fit page")),
-    singleMode(true), coverPageMode(true) {
-}
+// Constructeur de la classe : Initialisation de ses méthodes, hérite de QObject
+Book::Book() : 
+    QObject(), pathToDir(""), currPage(0), totalPage(0), ratio(QString("Fit page")), singleMode(true), coverPageMode(true) {}
 
+// Destructeur de la classe
 Book::~Book() {
 }
 
+/*
+Définit "path" comme chemin du repértoire contenant les images du livres
+*/
 void Book::setPathToDir(QString path) {
     pathToDir = path;
-    QDir dir(path);
-    dir.setNameFilters(Image::imageFilters);
-    foreach (QFileInfo var, dir.entryInfoList()) {
-        tabPathToImage.append(var.absoluteFilePath());
-        totalPage = totalPage + 1;
+    QDir dir(path);//crée 'dir'
+    dir.setNameFilters(Image::imageFilters);//définit filtre de nom de fichiers avec ceux de la classe Image::imagesFilters
+    foreach (QFileInfo var, dir.entryInfoList()) //itère sur chaque fichier d'info sur les entrée de dir
+    {
+        tabPathToImage.append(var.absoluteFilePath());//liste qui contient les chemins de toutes les images du livre
+        totalPage = totalPage + 1;//Nb total de page
     }
     changeCurrImage();
     emit pageChanged(true);
 }
 
 void Book::setRatio(QString r) {
-    ratio = r;
+    ratio = r;//stocke mode de rapport d'affichage de l'image ("Fit page" ou "Stretch")
 }
 
-void Book::setSingleMode(bool val) {
-    singleMode = val;
+// Modifie le mode d'affichage vers le mode 'une seule page'
+void Book::setSingleMode(bool value) {
+    singleMode = value;
     changeCurrImage();
     emit pageChanged(false);
 }
 
-void Book::setCoverPageMode(bool val) {
+// Modifie le mode de la page de couverture du livre électronique.
+void Book::setCoverPageMode(bool value) {
     setSingleMode(false);
-    coverPageMode = val;
-    if (coverPageMode && currPage != 0) {
+    coverPageMode = value;
+    if ((coverPageMode && currPage) != 0)//vérifie si le mode de page de couverture activé & si page actuelle pas première page.
+    {
         currPage = currPage - 1 + (currPage % 2);
     } else {
         currPage = currPage - (currPage % 2);
@@ -59,6 +66,7 @@ void Book::setCurrPage(int val) {
     }
 }
 
+// Mise à jour de l'image  affichée en fonction du mode de page et de la page actuelle
 void Book::changeCurrImage() {
     if (singleMode || currPage==totalPage-1 || (!singleMode && coverPageMode && currPage==0)) {
         currImage = QPixmap(tabPathToImage[currPage]);
@@ -67,7 +75,7 @@ void Book::changeCurrImage() {
     }
 }
 
-
+// Passage à la page suivante
 void Book::next() {
     if ((currPage<totalPage-1 && singleMode) || currPage<totalPage-2 ) {
         if (singleMode || currPage+1==totalPage-1 || (!singleMode && currPage==0 && coverPageMode)) {
@@ -84,6 +92,7 @@ void Book::next() {
     }
 }
 
+// Revenir à la page précédente
 void Book::previous() {
     if (currPage>0) {
         if (singleMode || currPage-1==0) {
